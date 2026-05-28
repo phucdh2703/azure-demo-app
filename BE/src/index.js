9a5c2a634fb37429cc2ac1ccc7a715059ce51b9c
phucdh <phucdh@mindsupernova.com>
2026-05-28 18:12:42 +0700

    Update workflows fe

--- BE/src/index.js
+++ BE/src/index.js
@@ -6,7 +6,12 @@ const employees = require('./routes/employees');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://stmyappdemo.z22.web.core.windows.net'
+  ]
+}));
 app.use(express.json());
 
 app.use('/api/employees', employees);
